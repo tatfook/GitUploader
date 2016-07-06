b0c4001bdf8b1c1e82e57bbfc64232612212fe83
Li, Xizhi <lixizhi@yeah.net>
2016-07-06 17:39:52 +0800

    reviewed

--- timeTransform.lua
+++ timeTransform.lua
@@ -4,21 +4,21 @@ date: 2016/07/04
 desc: time format transform £¨example£º4000 s --> 1 h 6 m 40 s£©
 usage:
 ----------------------------------------------------------
-NPL.load("(gl)scritp/ide/System/Algorithm/timeTransform.lua");
-local timeTransfrom = commonlib.gettable("System/Algorithm/timeTransform.lua");
+NPL.load("(gl)script/ide/System/Algorithm/timeTransform.lua");
+local timeTransfrom = commonlib.gettable("System.Algorithm.timeTransform");
 timeTransform.trans(secs);
 ----------------------------------------------------------
 --]]
+local string_find = string.find;
+local timeTransfrom = commonlib.gettable("System.Algorithm.timeTransform");
 
-local timeTransfrom = commonlib.gettable("System/Algorithm/timeTransform.lua");
-
-local function timeTransfrom.string_split(str, exp)
+function timeTransfrom.string_split(str, exp)
     local result = {};
     local strlen = #str;
     local index = 1;
     while index < strlen do
-        local start, end = string.find(str, exp, index);
-        if start == nil then
+        local start, end = string_find(str, exp, index);
+        if (not start) then
             break;
         else
             local match_str = string.sub(str, index, start - 1);
@@ -32,18 +32,21 @@ local function timeTransfrom.string_split(str, exp)
      
     return result;
 end
- 
-local function timeTransfrom.format_time(secs, format)
-    local format = string_split(format, ":")
-    local radix = {24, 60, 60};
-    local time_str = "";
+local string_split = timeTransfrom.string_split;
+
+local s_radix = {24, 60, 60};
+
+-- @param sFormat: string
+function timeTransfrom.format_time(nSecs, sFormat)
+    local arrayFormat = string_split(sFormat, ":")
+    local time_str = ""; 
     local base_value, base_name, value;
-    local i = #radix;
+    local i = #s_radix;
     while i > 0 do
-        base_value = radix[i];
-        base_name = format[i + 1];
+        base_value = s_radix[i];
+        base_name = arrayFormat[i + 1];
          
-        value = secs % base_value;
+        value = nSecs % base_value;
          
         if value > 0 then
             if base_name then
@@ -51,18 +54,47 @@ local function timeTransfrom.format_time(secs, format)
             end
         end
          
-        secs = math.floor(secs / base_value);
+        nSecs = math.floor(nSecs / base_value);
          
         i = i - 1;
     end
      
     if secs > 0 then
-        time_str = secs .. format[i + 1] .. time_str;
+        time_str = secs .. arrayFormat[i + 1] .. time_str;
     end
      
     return time_str;
 end
  
 function timeTransform.trans(secs) 
-    print(format_time(secs, "day:hour:minute:second"));
+    log(format_time(secs, "day:hour:minute:second"));
 end
+
+-- @param text: the text to print.
+-- @param formatter: nil or function(text)  end, which returns the formated text. 
+local function PrintMe(text, formatter)
+    log(formatter and formatter(text) or text);
+end
+
+
+-- dynamic scoping(C,c++java, C#, ) <  static scoping (lua, js, python)
+
+-- closures
+local function test_print()
+    PrintMe("abc");
+    local i = 0;
+    local function f(text)
+        i = i + 1;
+        return string.upper(text or "") .. " " ..tostring(i);
+    end;
+    for k=1, 3 do
+        commonlib.TimerManager.SetTimeout(function()
+            PrintMe("abc", f);
+        end, 1000);
+    end
+    log("starting....\n");
+end
+test_print();
+-->abc
+-->starting...
+-->ABC 1 -->ABC 2 -->ABC 3
